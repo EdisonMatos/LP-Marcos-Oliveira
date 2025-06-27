943c00485f52a4039a39a1247d1c0dda12e34624
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-27 16:57:42 -0300

    fix: bgSectionDark com tom de Azul da Logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export const theme = {
       // Fundos de Seção
 
       bgSectionLight: "#FFFFFF",
-      bgSectionDark: "#000080",
+      bgSectionDark: "#061932",
       bgSectionOpacityLight: "#F7F7F7",
       bgFixedLight: "#E8E8E8",
       bgSectionOpacityDark: "#111111",
